ad136b42c6f13b90cc01d97608a0df596912d80b
ericc70 <eric070@net-c.fr>
2022-06-23 16:35:56 +0200

    add componzents

---
+++ web3-domains/src/components/input-form.vue
@@ -0,0 +1,58 @@
+<template>
+  <div className="form-container">
+    <div className="first-row">
+      <input type="text" placeholder="domain" v-model="domain" />
+      <p className="tld">{{ tld }}</p>
+    </div>
+
+    <input type="text" v-model="record" placeholder="whats ur power" />
+
+    <div className="button-container">
+      <button className="cta-button mint-button" @click="mintDomain">
+        Mint
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+import {ethers} from "ethers";
+import contractAbi from "../../../artifacts/contracts/Domains.sol/Domains"
+// import contract
+const domain = ref("")
+const reccord = ref("")
+const tld = ref("Sdomain")
+const CONTRACT_ADRESS = ""
+
+
+async function mintDomain(){
+    if(!domain.value){
+        return
+    }
+    if(domain.value.length <3)
+    {
+        alert("domain must be longer 3")
+        return
+    }
+
+    const price = domain.length == 3 ? "0.5" : domain.length == 4 ? "0.3" : "0.1"
+    console.log("minting domain" , domain, "wirh price", price )
+
+    try {
+       const {ethereum} = window
+
+       if(ethereum){
+           const provider = new ethers.providers.Web3Provider(ethereum)
+       }
+
+
+    } catch (error) {
+        console.error(error)
+    }
+
+
+}
+
+</script>
---
+++ web3-domains/src/components/layout.vue
@@ -0,0 +1,22 @@
+<script setup>
+const TWITTER_HANDLE = "";
+const TWITTER_LINK = "";
+</script>
+
+<template>
+  <div className="App">
+    <div className="container">
+      <div className="header-container">
+        <header>
+          <div className="left">
+            <p className="title"> Name Service web3</p>
+            <p className="subtitle">Your immortal API on the blockchain!</p>
+          </div>
+        </header>
+      </div>
+      <slot></slot>
+
+
+    </div>
+  </div>
+</template>
